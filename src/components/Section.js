4e12466d6b8777960e64e58c0f23487ef1e37c12
luis augsburger <luis.augsburger@gmail.com>
2022-04-23 21:14:04 -0300

    signup inputs

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import { Login } from './Login'
 import { Signup } from './Signup'
 
+const INITIAL_USER = [
+    {
+        name: 'Luis',
+        lastname: 'Augsburger',
+        email: 'test@test.com',
+        password: '1234',
+        userType: 'student'
+    }
+]
+
 export const Section = () => {
 
     const [showLogin, setshowLogin] = useState(true)
+    const [userRegistered, setUserRegistered] = useState(INITIAL_USER)
 
 
     const handleForm = () => {
@@ -14,7 +25,7 @@ export const Section = () => {
     return (
         <section className="mx-auto w-[927px] bg-white rounded-[10px] shadow-lg px-20 py-20">
             {
-                showLogin ? <Login  handleForm={handleForm}/> : <Signup handleForm={handleForm} />
+                showLogin ? <Login handleForm={handleForm} /> : <Signup handleForm={handleForm} />
             }
         </section>
     )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,7 @@ import { Button } from "./Button"
 import { Subtitle } from "./Subtitle"
 
 
-export const Signup = ({handleForm}) => {
+export const Signup = ({ handleForm }) => {
 
     const [name, setName] = useState('')
     const [lastname, setLastname] = useState('')
@@ -14,10 +14,27 @@ export const Signup = ({handleForm}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        console.log(name, lastname, email, password, userType)
     }
 
-    const handleName = () => {
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
 
+    const handleLastName = (e) => {
+        setLastname(e.target.value)
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleUserType = (e) => {
+        setUserType(e.target.value)
     }
 
     return (
@@ -25,20 +42,52 @@ export const Signup = ({handleForm}) => {
             <Subtitle text="Please complete the form with your data" />
             <form className="w-[750px]" onSubmit={handleSubmit}>
                 <label className="block text-xl text-[#ccc]" htmlFor="userName">Name</label>
-                <input className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]" type="text" placeholder="Enter your name..." id="userName" autoFocus/>
+                <input
+                    className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]"
+                    type="text"
+                    placeholder="Enter your name..."
+                    id="userName"
+                    autoFocus
+                    onChange={handleName}
+                />
                 <label className="block text-xl text-[#ccc]" htmlFor="userLastName">Last name</label>
-                <input className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]" type="text" placeholder="Enter your lastname..." id="userLastName"/>
+                <input
+                    className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]"
+                    type="text"
+                    placeholder="Enter your lastname..."
+                    id="userLastName"
+                    onChange={handleLastName}
+                />
                 <label className="block text-xl text-[#ccc]" htmlFor="userEmail">Email address</label>
-                <input className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]" type="email" placeholder="Enter your email address..." id="userEmail"/>
+                <input
+                    className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]"
+                    type="email"
+                    placeholder="Enter your email address..."
+                    id="userEmail"
+                    onChange={handleEmail}
+                />
                 <label className="block text-xl text-[#ccc]" htmlFor="userPassword">Password</label>
-                <input className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]" type="password" placeholder="Enter your pasword..." id="userPassword"/>
+                <input
+                    className="block text-xl p-2 w-full border border-[#ccc] rounded-[10px] mt-2 mb-4 text-[#333]"
+                    type="password"
+                    placeholder="Enter your pasword..."
+                    id="userPassword"
+                    onChange={handlePassword}
+                />
                 <label className="block text-xl text-[#ccc]" htmlFor="optionUser">Category</label>
-                <select id="optionUser" className="block text-xl p-2 mt-2 mb-10 w-full border border-[#ccc] rounded-[10px] text-[#333]" htmlFor="userPassword">
-                    <option value="" defaultValue></option>
+                <select
+                    className="block text-xl p-2 mt-2 mb-10 w-full border border-[#ccc] rounded-[10px] text-[#333]"
+                    id="optionUser"
+                    htmlFor="userPassword"
+                    onChange={handleUserType}
+                >
+                    <option value="" disabled></option>
                     <option value="professor">Professor</option>
                     <option value="student">Student</option>
                 </select>
-                <Button text="Sign Up"/>
+                <Button
+                    text="Sign Up"
+                />
             </form>
         </>
     )
